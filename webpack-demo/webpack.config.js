9e206dbe06860e0aa6e9f13d0c2d6ef210ef3f81
Robby Tarigan <robbytarigan@hotmail.com>
2018-07-16 22:04:09 +0100

    Hot module replacement.

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,16 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        app: './src/index.js'
     },
-    devtool : 'inline-source-map',
-    plugins : [
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
         new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({ title : 'Output Management'})
+        new HtmlWebpackPlugin({ title: 'Output Management' }),
+        new webpack.HotModuleReplacementPlugin()
     ],
     output: {
         filename: '[name].bundle.js',
