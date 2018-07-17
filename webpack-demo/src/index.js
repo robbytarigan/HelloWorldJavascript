6ffebc2e688bd3a84b9ff41e1e56b741f4818ccc
Robby Tarigan <robbytarigan@hotmail.com>
2018-07-17 23:11:32 +0100

    Code Splitting: Dynamic loading with async.

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,13 +1,10 @@
-function getComponent() {
-  return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
-    var element = document.createElement('div');
-    var _ = _.default;
+async function getComponent() {
+  var element = document.createElement('div');
+  const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
-
-  }).catch(error => 'An error occurred while loading the component');
+  return element;
 }
 
 getComponent().then(component => {
